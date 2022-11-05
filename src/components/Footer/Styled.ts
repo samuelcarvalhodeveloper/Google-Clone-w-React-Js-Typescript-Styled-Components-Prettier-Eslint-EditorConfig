a44edf0a2231590392060b69d156854d78ea6c77
Samuel Carvalho <93744509+samueldecarvalhodev@users.noreply.github.com>
2022-11-04 22:32:14 -0300

    footer  display flex

--- src/components/Footer/Styled.ts
+++ src/components/Footer/Styled.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const FooterStyled = styled.footer`
-  width: 100%;
+  min-width: 100%;
   position: absolute;
   bottom: 0;
   left: 0;
@@ -22,18 +22,41 @@ const FooterStyled = styled.footer`
       padding: 1.5rem 2.5rem;
       display: flex;
       justify-content: space-between;
+    .footer__more {
+      display: flex;
+      justify-content: center;
+      align-items: center;
 
       .footer__link {
-          font-size: 1.4rem;
-          color: #70757a;
-          text-decoration: none;
-          padding: 1.5rem;
-          display: inline-block;
-        }
+        font-size: 1.4rem;
+        color: #70757a;
+        text-decoration: none;
+        padding: 1.5rem;
+        display: inline-block;
+      }
 
       .footer__link:hover {
         text-decoration: underline;
       }
+    }
+
+    .footer__settings{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .footer__link {
+        font-size: 1.4rem;
+        color: #70757a;
+        text-decoration: none;
+        padding: 1.5rem;
+        display: inline-block;
+      }
+
+      .footer__link:hover {
+        text-decoration: underline;
+      }
+    }
   }
 
   @media screen and (min-width: 665px) and (max-width: 1200px) {
